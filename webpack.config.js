f3cf046ca391435fa18070e084d3ea413634f0fe
sofiamatulis <sosomatulis@hotmail.com>
2018-03-01 13:06:54 -0500

    updated package json port

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
+    port: process.env.PORT,
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'src'),
