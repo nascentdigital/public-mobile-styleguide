b5037a8c3d256e5ba5bebd206f7df69df7243bff
sofiamatulis <sosomatulis@hotmail.com>
2018-03-03 21:52:46 -0500

    added procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: styleguidist server ./src/index.js
--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-var express = require("express");
-var app = express();
-
-// Set up a URL route
-app.get("/", function(req, res) {
- res.send("Heroku Demo!");
-});
-
-// bind the app to listen for connections on a specified port
-var port = process.env.PORT || 6060;
-app.listen(port);
-
-// Render some console log output
-console.log("Listening on port " + port);
+// var express = require("express");
+// var app = express();
+// // var MainButton = require('../src/components/MainButton');
+// //
+// // // Set up a URL route
+// // app.get("/", function(req, res) {
+// //  res.send("Heroku Demo!");
+// // });
+// //
+// // // bind the app to listen for connections on a specified port
+// // var port = process.env.PORT || 6060;
+// // app.listen(port);
+// //
+// // // Render some console log output
+// // console.log("Listening on port " + port);
+//
+// module.exports = {
+//   configureServer(app) {
+//     // `app` is the instance of the express server running Styleguidist
+//     app.get('/', (req, res) => {
+//       res.status(200).send({ response: 'Server invoked' })
+//     })
+//
+//     var port = process.env.PORT || 6060;
+//     app.listen(port);
+//
+//     // Render some console log output
+//     console.log("Listening on port " + port);
+//   }
+// }
